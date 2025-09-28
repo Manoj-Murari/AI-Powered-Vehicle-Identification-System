29f362ecf97e3732bcc79ae2196a839b31649e2a
Murari Venkata Sai Manoj <saimnj357@gmail.com>
2025-09-29 00:08:56 +0530

    Update detect.py

--- python_service/detect.py
+++ python_service/detect.py
@@ -8,8 +8,8 @@ video_path = sys.argv[1]
 video_source = VideoFile(video_path, samples_per_second=1)
 frames = [f.image for f in video_source]
 
-predictor = Predictor("e001c156-5de0-43f3-9991-f19699b31202", api_key="land_sk_aMemWbpd41yXnQ0tXvZMh59ISgRuKNRKjJEIUHnkiH32NBJAwf")
-ocr = OcrPredictor(api_key="land_sk_WVYwP00xA3iXely2vuar6YUDZ3MJT9yLX6oW5noUkwICzYLiDV")
+predictor = Predictor("e001c156-5de0-43f3-9991-f19699b31202", api_key={Your API Key Here})
+ocr = OcrPredictor(api_key="{Your API Key Here}")
 
 bounding_boxes = [predictor.predict(frame) for frame in frames]
 cropped_imgs = [crop(bboxes, frame) for frame, bboxes in zip(frames, bounding_boxes)]
